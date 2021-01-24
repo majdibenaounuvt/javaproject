0385db6f5e30c0fda34d46c504f3ccd9916c218b
majdibenaounuvt <etudiant.benaoun.mohamedmajdi@uvt.tn>
2021-01-24 22:14:32 +0100

    our first commit

---
+++ gestion des produit/src/gestion/des/produit/modele/Client.java
@@ -0,0 +1,16 @@
+package gestion.des.produit.modele;
+
+
+public class Client {
+    public String nom_prenom;
+   public String telephone ;
+   public String adresse;
+
+    public Client(String nom_prenom, String telephone, String adresse) {
+        this.nom_prenom = nom_prenom;
+        this.telephone = telephone;
+        this.adresse = adresse;
+    }
+    
+    
+}
